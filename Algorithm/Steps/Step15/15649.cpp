d9866f29aacd385e5271abf07a1ee423821163cf
HyeonMyeongJang <gkagm2@gmail.com>
2020-02-05 23:14:40 +0900

    solved

--- Algorithm/Steps/Step15/15649.cpp
+++ Algorithm/Steps/Step15/15649.cpp
@@ -2,31 +2,36 @@
 #include <vector>
 using namespace std;
 
-int n, m;
-vector<int> v;
 bool visit[9];
+vector<int> vec;
+int n, m;
 
-void solve(int cnt) {
+void Solve(int cnt) {
 	if (cnt == m) {
-		for (int i = 0; i < v.size(); i++)
-			cout << v[i] << " ";
-		cout << "\n";
+		for (int i = 0; i < vec.size(); ++i) {
+			cout << vec[i] << " ";
+		}
+		cout << '\n';
 		return;
 	}
 
-	for (int i = 1; i <= n; i++) {
-		if (visit[i]) continue;
-		visit[i] = 1;
-		v.push_back(i);
-		solve(cnt + 1);
-		visit[i] = 0;
-		v.pop_back();	
+	for (int i = 1; i <= n; ++i) {
+		if (visit[i])
+			continue;
+		visit[i] = true;
+		vec.push_back(i);
+		Solve(cnt + 1);
+		visit[i] = false;
+		vec.pop_back();
 	}
 }
+
 int main() {
-	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
 	cin >> n >> m;
-	solve(0);
+	Solve(0);
+
 	return 0;
 }
