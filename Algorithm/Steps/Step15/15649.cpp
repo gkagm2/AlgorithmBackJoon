72cdff4a8db44163ddfca2c5d92db53d514d85a1
HyeonMyeongJang <gkagm2@gmail.com>
2020-02-05 20:25:40 +0900

    해결완료

--- Algorithm/Steps/Step15/15649.cpp
+++ Algorithm/Steps/Step15/15649.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-	
+int n, m;
+vector<int> v;
+bool visit[9];
 
+void solve(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visit[i]) continue;
+		visit[i] = 1;
+		v.push_back(i);
+		solve(cnt + 1);
+		visit[i] = 0;
+		v.pop_back();	
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> m;
+	solve(0);
 	return 0;
 }
