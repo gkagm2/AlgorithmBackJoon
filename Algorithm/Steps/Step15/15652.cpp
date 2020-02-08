8434ba1317e0a82ae2af2edae94d8ff2fcb4fdff
HyeonMyeongJang <gkagm2@gmail.com>
2020-02-08 15:48:44 +0900

    solved

---
+++ Algorithm/Steps/Step15/15652.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, x;
+vector<int > vec;
+
+void DFS(int index, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < vec.size(); ++i) {
+			cout << vec[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = index; i <= n; ++i) {
+		vec.push_back(i);
+		DFS(i, cnt + 1);
+		vec.pop_back();
+	}
+}
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cin >> n >> m;
+	DFS(1, 0);
+	return 0;
+}
