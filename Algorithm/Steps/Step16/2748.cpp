dca2e8d1bbfe7822fff91adec7e226ce1174fa57
HyeonMyeongJang <gkagm2@gmail.com>
2020-02-11 21:01:19 +0900

    solved

---
+++ Algorithm/Steps/Step16/2748.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+
+	long long arr[91];
+
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i = 2; i <= 90; ++i) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	cin >> n;
+	cout << arr[n];
+
+	return 0;
+}
