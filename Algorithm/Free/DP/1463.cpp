438d4e151075722d4d7e90176dd4bc4c01775f26
HyeonMyeongJang <gkagm2@gmail.com>
2020-08-28 15:23:50 +0900

    Top down, Bottom up add

--- Algorithm/Free/DP/1463.cpp
+++ Algorithm/Free/DP/1463.cpp
@@ -1,8 +1,10 @@
+// Bottom up (loop)
+/*
 #include <iostream>
 #include <cstring>
 #include <algorithm>
 using namespace std;
-// memorization
+// Memoization
 int arr[1000001];
 int main() {
 	cin.tie(NULL);
@@ -24,5 +26,43 @@ int main() {
 	}
 	
 	cout << arr[n];
+	return 0;
+}
+*/
+
+
+// Top down  (recursive)
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+// Memoization
+int arr[1000001];
+
+int go(int n) {
+	if (n == 1) return 0;
+	if (arr[n] > 0) return arr[n];
+	arr[n] = go(n - 1) + 1;
+	if (n % 2 == 0) {
+		int temp = go(n / 2) + 1;
+		if (arr[n] > temp) arr[n] = temp;
+	}
+	if (n % 3 == 0) {
+		int temp = go(n / 3) + 1;
+		if (arr[n] > temp) arr[n] = temp;
+	}
+	return arr[n];
+}
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	memset(arr, 0, sizeof(int) * (n + 1));
+	cout << go(n);
+
 	return 0;
 }
