bc45d3b9d8a5661c326b8e9257bfdc7f99b57cb0
HyeonMyeongJang <gkagm2@gmail.com>
2020-09-01 19:35:32 +0900

    Solved

---
+++ Algorithm/Free/DP/1912.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[100001];
+int d[100001];
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i) {
+		cin >> a[i];
+	} 
+
+	d[1] = a[1];
+	for (int i = 2; i <= n; ++i) {
+		d[i] = max(max(0, d[i - 1]) + a[i], a[i]);
+	}
+
+	cout << *max_element(d + 1, d + n + 1);
+	return 0;
+}
