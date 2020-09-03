00d4fc4472d83e192eeaf1725ec7d21a1f924ef5
HyeonMyeongJang <gkagm2@gmail.com>
2020-09-03 12:40:23 +0900

    Solved in another way

--- Algorithm/Steps/Step10/1978.cpp
+++ Algorithm/Steps/Step10/1978.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 using namespace std;
 
+// O(root of n)
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n, count = 0;
+	int num;
+	bool isPrimeNum;
+
+	cin >> n;
+
+	while (n--) {
+		isPrimeNum = true;
+		cin >> num;
+		
+		if (num < 2) {
+			isPrimeNum = false;
+		}
+		else {
+			for (int i = 2; i *i <= num; ++i) {
+				if (num % i == 0) {
+					isPrimeNum = false;
+					break;
+				}
+			}
+		}
+
+		if (isPrimeNum)
+			++count;
+	}
+	cout << count;
+
+	return 0;
+}
+
+// O(n)
+/*
 int main()
 {
 	cin.tie(NULL);
@@ -29,3 +66,4 @@ int main()
 
 	return 0;
 }
+*/
