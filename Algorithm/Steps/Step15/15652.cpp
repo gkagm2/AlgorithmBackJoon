029f0b1529f033f8552c23a4a58723fcf518e622
HyeonMyeongJang <gkagm2@gmail.com>
2020-09-10 00:04:17 +0900

    solved by dfs

--- Algorithm/Steps/Step15/15652.cpp
+++ Algorithm/Steps/Step15/15652.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+using namespace std;
+
+int a[10];
+
+void dfs(int idx, int start, int n, int m) {
+	if (idx == m) {
+		for (int i = 0; i < m; ++i) {
+			cout << a[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = start; i <= n; ++i) {
+		a[idx] = i;
+		dfs(idx + 1, i,  n, m);
+	}
+}
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int n, m;
+
+	cin >> n >> m;
+	dfs(0, 1, n, m);
+
+	return 0;
+}
+
+/*
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -29,3 +62,4 @@ int main() {
 	DFS(1, 0);
 	return 0;
 }
+*/
