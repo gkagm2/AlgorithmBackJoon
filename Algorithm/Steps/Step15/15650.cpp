a4e7b62652e9d445ea694a3b87e8a36af985338a
HyeonMyeongJang <gkagm2@gmail.com>
2020-09-09 23:41:10 +0900

    Solved

--- Algorithm/Steps/Step15/15650.cpp
+++ Algorithm/Steps/Step15/15650.cpp
@@ -1,3 +1,39 @@
+// time : 2^n
+#include <iostream>
+using namespace std;
+
+bool visit[10];
+int a[10];
+
+void go(int idx, int selected, int n, int m) {
+	if (selected == m) {
+		for (int i = 0; i < m; ++i) {
+			cout << a[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	
+	if (idx> n) return;
+	a[selected] = idx;
+	go(idx + 1, selected + 1, n, m);
+	a[selected] = 0;
+	go(idx + 1, selected, n, m);
+}
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	int n, m;
+	cin >> n >> m;
+
+	go(1, 0, n, m);
+
+	return 0;
+}
+
+// time : n!
+/*
 #include <iostream>
 using namespace std;
 
@@ -34,6 +70,7 @@ int main() {
 
 	return 0;
 }
+*/
 
 /*
 #include <iostream>
