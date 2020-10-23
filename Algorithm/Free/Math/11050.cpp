cca0b054de653f4cd42d67551f1c7c620b4a2426
HyeonMyeongJang <gkagm2@gmail.com>
2020-10-24 00:20:57 +0900

    Solved (+dp)

--- Algorithm/Free/Math/11050.cpp
+++ Algorithm/Free/Math/11050.cpp
@@ -1,4 +1,28 @@
-// DP ¾È½áµµ µÊ
+// Used DP
+#include <iostream>
+using namespace std;
+
+int dp[11];
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	dp[0] = 1;
+	for (int i = 1; i < 11; ++i) {
+		dp[i] = dp[i - 1] * i;
+	}
+
+
+	int n, k;
+	cin >> n >> k;
+
+	cout << dp[n] / (dp[n - k] * dp[k]);
+
+	return 0;
+}
+
+/*
 #include <iostream>
 using namespace std;
 
@@ -21,3 +45,4 @@ int main() {
 
 	return 0;
 }
+*/
