0a572266fa3a05221523e7c864032507e6fd75e5
HyeonMyeongJang <gkagm2@gmail.com>
2022-05-02 05:35:36 +0900

    solved

---
+++ Algorithm/Steps/Step1/10926.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	string str;
+	string back = "??!";
+	cin >> str;
+
+	cout << str << back;
+
+	return 0;
+}
