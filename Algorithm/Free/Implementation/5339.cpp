ecd9967e35c20ca60fc4874f284f5761be2e094b
HyeonMyeongJang <gkagm2@gmail.com>
2022-07-06 21:18:46 +0900

    solved

---
+++ Algorithm/Free/Implementation/5339.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	string pic = 
+		"     /~\\\n"
+		"    ( oo|\n"
+		"    _\\=/_\n"
+		"   /  _  \\\n"
+		"  //|/.\\|\\\\\n"
+		" ||  \\ /  ||\n"
+		"============\n"
+		"|          |\n"
+		"|          |\n"
+		"|          |";
+		
+	cout << pic;
+
+	return 0;
+}
