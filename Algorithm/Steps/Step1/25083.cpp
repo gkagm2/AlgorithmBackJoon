b7ac329f2a588875bb189d493b416d3dbed5e554
HyeonMyeongJang <gkagm2@gmail.com>
2022-07-07 16:07:26 +0900

    solved

---
+++ Algorithm/Steps/Step1/25083.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+
+	return 0;
+}
