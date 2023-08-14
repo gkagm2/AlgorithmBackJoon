7a14021bc4db7b14604658c7a6e5d6455ff0277d
HyeonMyeongJang <gkagm2@gmail.com>
2023-08-14 20:37:33 +0900

    solved

---
+++ Algorithm/Free/Implementation/3046.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int r1, r2, s;
+	cin >> r1 >> s;
+	r2 = 2 * s - r1;
+	cout << r2;
+	return 0;
+}
